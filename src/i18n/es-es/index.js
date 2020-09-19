ab5f3a7fd585d08e24a0ee2d14e1eefe808b28ff
Romario Huebra <romarioj2h@gmail.com>
2020-09-18 21:30:24 -0300

    Adding pt-br and es-es support

---
+++ src/i18n/es-es/index.js
@@ -0,0 +1,23 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+    failed: 'Acción fallida',
+    success: 'La acción fue exitosa',
+    index: {
+        edit: 'Editar',
+        delete: 'Borrar',
+        deleted: "Borrado con exito!"
+    },
+    editor: {
+        title: "Título",
+        content: "Contenido",
+        saved: "Guardado con exito!",
+        fieldIsRequired: "Campo requiredo"
+    },
+    layout: {
+        home: {
+            caption: "Vea todas tus notas!"
+        }
+    }
+}
---
+++ src/i18n/pt-br/index.js
@@ -0,0 +1,23 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+    failed: 'Ação falhou',
+    success: 'Ação bem sucedida',
+    index: {
+        edit: 'Editar',
+        delete: 'Apagar',
+        deleted: "Apagado com sucesso!"
+    },
+    editor: {
+        title: "Título",
+        content: "Conteúdo",
+        saved: "Salvo com sucesso!",
+        fieldIsRequired: "Campo obrigatório"
+    },
+    layout: {
+        home: {
+            caption: "Veja todas suas notas!"
+        }
+    }
+}
